a86f587fba63ac8620715d5827c6b9bd4251cb39
Pieter Beernink <60547491+pieterbnl@users.noreply.github.com>
2022-02-22 16:15:08 +0100

    create superhero service (interface)

---
+++ BlazorFullStackCrud/Client/Services/SuperHeroService/SuperHeroService.cs
@@ -0,0 +1,29 @@
+﻿namespace BlazorFullStackCrud.Client.Services.SuperHeroService
+{
+    public class SuperHeroService : ISuperHeroService
+    {
+        private readonly HttpClient _http;
+
+        public SuperHeroService(HttpClient http)
+        {
+            _http = http;
+        }
+        public List<SuperHero> Heroes { get; set; } = new List<SuperHero>();
+        public List<Comic> Comics { get; set; } = new List<Comic>();
+
+        public Task GetComics()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<SuperHero> GetSingleHero(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task GetSuperHeroes()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
